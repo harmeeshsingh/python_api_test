ef8dd2510a39ad1db58c0cd733930b383b7d3787
harmeeshsingh <harmeeshsingheee@gmail.com>
2025-07-24 18:57:24 +0530

    adding info in Readme.md

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # python_api_test
 This python code helps to retrieve the information of a pokemon from the pokiapi 
+
+One can add the list of the parameters to retive the other informatino of the pokemon.
+
+Using this code we are calling the following parameters currently 
+    Hight
+    weight
+    order
---
+++ python_api.py
@@ -0,0 +1,24 @@
+#information using python
+import requests
+base_url = "https://pokeapi.co/api/v2/"
+
+def get_pokemon_info(name):
+    url = f"{base_url}/pokemon/{name}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        pokemon_data = response.json()
+        #print(pokemon_data)
+        return pokemon_data
+    else:
+        print(f"Not able to reterive the data\nError: {response.status_code}" )
+    
+    
+
+pokemon_name = "bulbasaur"
+pokemon_info = get_pokemon_info(pokemon_name)
+
+if pokemon_info:
+    print(f"Name: {pokemon_info["name"]}")
+    print(f"Weight: {pokemon_info["weight"]}")
+    print(f"Order: {pokemon_info["order"]}")
+
